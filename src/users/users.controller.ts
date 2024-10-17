d06873fceec3027075c0344e225bc27a86a99d88
Luis-Moura <luish.moura2024@gmail.com>
2024-10-17 08:25:08 -0300

    feat(users): change password using the oldPassword

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { IChangePassword } from './models/changePassword.interface';
 import { IFindByEmail } from './models/findByEmail.interface';
 import { UsersService } from './users.service';
 
@@ -9,5 +18,17 @@ export class UsersController {
   @Get()
   async findByEmail(@Body() data: IFindByEmail) {
     return await this.usersService.findByEmail(data);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/change-password')
+  async changePassword(@Body() data: IChangePassword, @Request() req) {
+    const email = req.user.email;
+
+    const { oldPassword, newPassword } = data;
+
+    const newData = { email, oldPassword, newPassword };
+
+    return await this.usersService.changePassword(newData);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { IChangePassword } from './models/changePassword.interface';
 import { IFindByEmail } from './models/findByEmail.interface';
 
 @Injectable()
@@ -28,5 +30,28 @@ export class UsersService {
     }
 
     return { ...user, password: undefined };
+  }
+
+  async changePassword(data: IChangePassword) {
+    const user = await this.findByEmailUtil(data.email);
+
+    if (user) {
+      const isPasswordMatching = await bcrypt.compare(
+        data.oldPassword,
+        user.password,
+      );
+
+      if (isPasswordMatching) {
+        const hashedPassword = await bcrypt.hash(data.newPassword, 10);
+        user.password = hashedPassword;
+        await this.usersRepository.save(user);
+
+        return { message: 'Password changed successfully' };
+      }
+
+      throw new ConflictException('Invalid password');
+    }
+
+    throw new ConflictException('User not found');
   }
 }
