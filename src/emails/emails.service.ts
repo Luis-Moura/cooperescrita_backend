3ce4980c171041d13030db3c96291e845e10e483
Luis-Moura <luish.moura2024@gmail.com>
2024-10-21 07:18:16 -0300

    refactor(emails): extract common email sending logic to a private method

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -17,13 +17,12 @@ export class EmailsService {
     });
   }
 
-  async sendVerificationEmail(email: string, token: string) {
-    const url = `http://localhost:3000/verify-account?token=${token}`;
+  private async sendEmail(email: string, subject: string, html?: string) {
     try {
       await this.transporter.sendMail({
         to: email,
-        subject: 'Verificação de Email',
-        html: `Clique <a href="${url}">aqui</a> para verificar seu email.`,
+        subject,
+        html,
       });
     } catch (error) {
       console.log(error);
@@ -31,17 +30,15 @@ export class EmailsService {
     }
   }
 
+  async sendVerificationEmail(email: string, token: string) {
+    const url = `${process.env.BASE_URL}/verify-account?token=${token}`;
+    const html = `Clique <a href="${url}">aqui</a> para verificar sua conta.`;
+    await this.sendEmail(email, 'Verificação de Conta', html);
+  }
+
   async sendResetPasswordEmail(email: string, token: string) {
-    const url = `http://localhost:3000/reset-password?token=${token}`;
-    try {
-      await this.transporter.sendMail({
-        to: email,
-        subject: 'Redefinição de Senha',
-        html: `Clique <a href="${url}">aqui</a> para redefinir sua senha.`,
-      });
-    } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('Failed to send email');
-    }
+    const url = `${process.env.BASE_URL}/reset-password?token=${token}`;
+    const html = `Clique <a href="${url}">aqui</a> para redefinir sua senha.`;
+    await this.sendEmail(email, 'Redefinição de Senha', html);
   }
 }
