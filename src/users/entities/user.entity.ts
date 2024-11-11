e0903f630ffef8ce9fa87e499a6ffa1651c13b11
Luis-Moura <luish.moura2024@gmail.com>
2024-11-11 19:17:23 -0300

    feat(user): altera id para UUID

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -7,39 +7,43 @@ import {
 
 @Entity({ name: 'users' })
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string; // Alterado para string para UUID
 
+  // Informações Básicas do Usuário
   @Column()
   name: string;
 
-  @Column()
+  @Column({ unique: true })
   email: string;
 
   @Column()
   password: string;
 
-  @Column()
+  // Status do Usuário
+  @Column({ default: false })
   verified: boolean;
 
-  @CreateDateColumn()
-  createdAt: Date;
+  @Column({ type: 'enum', enum: ['user', 'admin'], default: 'user' })
+  role: string;
 
+  @Column({ nullable: true, default: false })
+  twoFA: boolean;
+
+  // Controle de Segurança e Login
   @Column({ default: 0 })
   failedLoginAttempts: number;
 
   @Column({ type: 'timestamp', nullable: true })
   lockUntil: Date | null;
 
-  @Column({ default: 'user' })
-  role: string;
-
-  @Column({ nullable: true, default: false })
-  twoFA: boolean;
-
+  // Verificação e Data de Criação
   @Column({ nullable: true })
   verificationCode: string;
 
   @Column({ nullable: true })
   verificationCodeExpires: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
 }
