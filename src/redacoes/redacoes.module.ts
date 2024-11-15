2fff07152fe99425d7d470bd26b46f595e6242e6
Luis-Moura <luish.moura2024@gmail.com>
2024-11-15 09:42:33 -0300

    feat(redacoes): integra entidade User ao módulo e serviço de Redações para validação de usuário nas operações de criação e busca

--- src/redacoes/redacoes.module.ts
+++ src/redacoes/redacoes.module.ts
@@ -3,9 +3,10 @@ import { RedacoesService } from './redacoes.service';
 import { RedacoesController } from './redacoes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Redacao } from './entities/redacao.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Redacao])],
+  imports: [TypeOrmModule.forFeature([Redacao, User])],
   controllers: [RedacoesController],
   providers: [RedacoesService],
 })
--- src/redacoes/redacoes.service.ts
+++ src/redacoes/redacoes.service.ts
@@ -3,20 +3,30 @@ import { CreateRedacaoDto } from './dto/create-redacao.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Redacao } from './entities/redacao.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class RedacoesService {
   constructor(
     @InjectRepository(Redacao)
     private readonly redacaoRepository: Repository<Redacao>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  async create(redacaoDto: CreateRedacaoDto, userId: string) {
+  async create(redacaoDto: CreateRedacaoDto, userId: string): Promise<Redacao> {
     if (!userId) {
       throw new Error('User not found');
     }
 
-    const redacao = this.redacaoRepository.create({
+    const user: User = await this.userRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const redacao: Redacao = this.redacaoRepository.create({
       ...redacaoDto,
       user: { id: userId },
     });
@@ -24,14 +34,26 @@ export class RedacoesService {
     return await this.redacaoRepository.save(redacao);
   }
 
-  async getRedacoes(userId: string) {
+  async getRedacoes(userId: string): Promise<Redacao[]> {
     if (!userId) {
       throw new Error('User not found');
     }
 
-    const redacoes = await this.redacaoRepository.find({
+    const user: User = await this.userRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const redacoes: Redacao[] = await this.redacaoRepository.find({
       where: { user: { id: userId } },
     });
+
+    if (redacoes.length === 0) {
+      throw new Error('Redacoes not found');
+    }
 
     return redacoes;
   }
