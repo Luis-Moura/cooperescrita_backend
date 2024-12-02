3ac97a1dae27169b36330d2d2b3fd331d1735f8c
Luis-Moura <luish.moura2024@gmail.com>
2024-12-02 09:42:38 -0300

    feat(redacoes): aprimora DTOs de redação com validações e exemplos na documentação da API

--- src/redacoes/dto/createDefinitiveRedacaoDto.ts
+++ src/redacoes/dto/createDefinitiveRedacaoDto.ts
@@ -2,20 +2,36 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
 
 export class createDefinitiveRedacaoDto {
-  @ApiProperty({ description: 'Título da redação', required: false })
+  @ApiProperty({
+    description: 'Título da redação',
+    required: false,
+    minLength: 0,
+    maxLength: 100,
+    example: '"Minha Redação"',
+  })
   @IsOptional()
   @IsString()
+  @Length(0, 100)
   title: string;
 
-  @ApiProperty({ description: 'Tema da redação' })
+  @ApiProperty({
+    description: 'Tema da redação',
+    required: true,
+    minLength: 5,
+    maxLength: 100,
+    example: '"Tema da Redação"',
+  })
   @IsNotEmpty()
   @IsString()
+  @Length(5, 100)
   topic: string;
 
   @ApiProperty({
     description: 'Conteúdo da redação',
+    required: true,
     minLength: 700,
     maxLength: 4000,
+    example: '"Conteúdo da Redação (tamanho min. 700 e tamanho máx. 4000)"',
   })
   @IsNotEmpty()
   @IsString()
--- src/redacoes/dto/createDraftRedacaoDto.ts
+++ src/redacoes/dto/createDraftRedacaoDto.ts
@@ -2,14 +2,28 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
 
 export class createDraftRedacaoDto {
-  @ApiProperty({ description: 'Título da redação', required: false })
+  @ApiProperty({
+    description: 'Título da redação',
+    required: false,
+    minLength: 0,
+    maxLength: 100,
+    example: '"Minha Redação"',
+  })
   @IsOptional()
   @IsString()
+  @Length(0, 100)
   title: string;
 
-  @ApiProperty({ description: 'Tema da redação', required: true })
+  @ApiProperty({
+    description: 'Tema da redação',
+    required: true,
+    minLength: 5,
+    maxLength: 100,
+    example: '"Tema da Redação"',
+  })
   @IsNotEmpty()
   @IsString()
+  @Length(5, 100)
   topic: string;
 
   @ApiProperty({
@@ -17,6 +31,7 @@ export class createDraftRedacaoDto {
     required: false,
     minLength: 0,
     maxLength: 4000,
+    example: '"Conteúdo da Redação (tamanho máx. 4000)"',
   })
   @IsOptional()
   @IsString()
