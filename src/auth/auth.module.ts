500b2455885ecd4e52b64d72eb011039b88d3d2d
Luis-Moura <luish.moura2024@gmail.com>
2024-12-06 09:23:59 -0300

    refactor(auth): substitui AuthService por serviços específicos de autenticação

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,13 +6,18 @@ import * as dotenv from 'dotenv';
 import { EmailsModule } from 'src/emails/emails.module';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
-import { AuthService } from './auth.service';
+// import { AuthService } from './auth.service';
 import { PasswordController } from './controllers/password.controller';
 import { SessionController } from './controllers/session.controller';
 import { SigninController } from './controllers/signin.controller';
 import { SignupController } from './controllers/signup.controller';
 import { VerificationController } from './controllers/verification.controller';
 import { RolesGuard } from './guards/roles.guard';
+import { PasswordService } from './services/password.service';
+import { SessionService } from './services/session.service';
+import { SignInService } from './services/signin.service';
+import { SignUpService } from './services/signup.service';
+import { VerificationService } from './services/verification.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 dotenv.config();
@@ -36,13 +41,24 @@ dotenv.config();
     SessionController,
   ],
   providers: [
-    AuthService,
+    PasswordService,
+    SessionService,
+    SignInService,
+    SignUpService,
+    VerificationService,
     UsersService,
     // EmailsService,
     LocalStrategy,
     JwtStrategy,
     RolesGuard,
   ],
-  exports: [AuthService],
+  exports: [
+    PasswordService,
+    SessionService,
+    SignInService,
+    SignUpService,
+    VerificationService,
+    UsersService,
+  ],
 })
 export class AuthModule {}
