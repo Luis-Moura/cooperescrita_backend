546e9a1548c2b541ebdcf60ff5420f4bb6d7d4c6
Luis-Moura <luish.moura2024@gmail.com>
2024-12-09 11:06:16 -0300

    refactor(app): remove configurações de banco de dados obsoletas do módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,11 +21,6 @@ const redisUrl = new URL(process.env.REDIS_URL || '');
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      // host: process.env.DATABASE_HOST,
-      // port: parseInt(process.env.DATABASE_PORT),
-      // username: process.env.DATABASE_USERNAME,
-      // password: process.env.DATABASE_PASSWORD,
-      // database: process.env.DATABASE_NAME,
       entities: [User, Redacao],
       synchronize: true,
     }),
