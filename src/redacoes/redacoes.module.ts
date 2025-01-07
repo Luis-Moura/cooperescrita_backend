537dacf5cef19c1df941e995681e7d4f78182152
Luis-Moura <luish.moura2024@gmail.com>
2025-01-07 10:32:30 -0300

    feat(redacao): adiciona controlador e serviço para deletar redação

--- src/redacoes/redacoes.module.ts
+++ src/redacoes/redacoes.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { CreateRedacaoController } from './controllers/createRedacao.controller';
+import { DeleteRedacaoController } from './controllers/deleteRedacao.controller';
 import { GetRedacaoController } from './controllers/getRedacao.controller';
 import { Redacao } from './entities/redacao.entity';
 import { CreateRedacaoService } from './services/createRedacao.service';
+import { DeleteRedacaoService } from './services/deleteRedacao.service';
 import { GetRedacaoService } from './services/getRedacao.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Redacao, User])],
-  controllers: [GetRedacaoController, CreateRedacaoController],
-  providers: [CreateRedacaoService, GetRedacaoService],
+  controllers: [
+    GetRedacaoController,
+    CreateRedacaoController,
+    DeleteRedacaoController,
+  ],
+  providers: [CreateRedacaoService, GetRedacaoService, DeleteRedacaoService],
 })
 export class RedacoesModule {}
