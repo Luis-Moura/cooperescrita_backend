62767b6b62f998673f6ae8d5909480bccbcb5f33
Luis-Moura <luish.moura2024@gmail.com>
2025-01-13 09:39:56 -0300

    refactor(redacao): melhora tratamento de erros e validações nos serviços de criação, deleção e obtenção de redações

--- src/redacoes/services/deleteRedacao.service.ts
+++ src/redacoes/services/deleteRedacao.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
@@ -13,30 +18,34 @@ export class DeleteRedacaoService {
   ) {}
 
   async deleteRedacaoById(userId: string, redacaoId: number): Promise<void> {
-    if (!userId) {
-      throw new NotFoundException('User not found');
+    try {
+      if (!userId) {
+        throw new NotFoundException('User not found');
+      }
+
+      if (!redacaoId || isNaN(redacaoId)) {
+        throw new BadRequestException('Invalid Id');
+      }
+
+      const user: User = await this.userRepository.findOne({
+        where: { id: userId },
+      });
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      const redacao: Redacao = await this.redacaoRepository.findOne({
+        where: { id: redacaoId, user: { id: userId } },
+      });
+
+      if (!redacao) {
+        throw new NotFoundException('Redacao not found');
+      }
+
+      await this.redacaoRepository.delete({ id: redacaoId });
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-
-    if (!redacaoId || isNaN(redacaoId)) {
-      throw new NotFoundException('Invalid Id');
-    }
-
-    const user: User = await this.userRepository.findOne({
-      where: { id: userId },
-    });
-
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
-
-    const redacao: Redacao = await this.redacaoRepository.findOne({
-      where: { id: redacaoId, user: { id: userId } },
-    });
-
-    if (!redacao) {
-      throw new NotFoundException('Redacao not found');
-    }
-
-    await this.redacaoRepository.delete({ id: redacaoId });
   }
 }
--- src/redacoes/services/getRedacao.service.ts
+++ src/redacoes/services/getRedacao.service.ts
@@ -1,6 +1,7 @@
 import {
   BadRequestException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -24,89 +25,97 @@ export class GetRedacaoService {
     offset: number,
     orderQuery: IOrderQuery,
   ): Promise<IGetRedacoes> {
-    if (!userId) {
-      throw new NotFoundException('User not found');
+    try {
+      if (!userId) {
+        throw new NotFoundException('User not found');
+      }
+
+      if (
+        isNaN(limit) ||
+        isNaN(offset) ||
+        limit === undefined ||
+        offset === undefined
+      ) {
+        throw new BadRequestException('Invalid limit or offset');
+      }
+
+      const user: User = await this.userRepository.findOne({
+        where: { id: userId },
+      });
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      let order = {};
+      const where: any = { user: { id: userId } };
+
+      if (orderQuery.order) {
+        order =
+          orderQuery.order === 'crescente'
+            ? { createdAt: 'ASC' }
+            : { createdAt: 'DESC' };
+      }
+
+      if (orderQuery.statusEnvio) {
+        where.statusEnvio = orderQuery.statusEnvio;
+      }
+
+      if (orderQuery.statusCorrecao) {
+        where.statusCorrecao =
+          orderQuery.statusCorrecao === 'corrigidas'
+            ? 'corrigida'
+            : 'nao_corrigida';
+      }
+
+      const redacoes: Redacao[] = await this.redacaoRepository.find({
+        where,
+        order,
+        take: limit,
+        skip: offset,
+      });
+
+      const totalRedacoes = await this.redacaoRepository.count({ where });
+
+      if (redacoes.length === 0) {
+        throw new NotFoundException('Redacoes not found');
+      }
+
+      return { redacoes, totalRedacoes };
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-
-    if (
-      isNaN(limit) ||
-      isNaN(offset) ||
-      limit === undefined ||
-      offset === undefined
-    ) {
-      throw new BadRequestException('Invalid limit or offset');
-    }
-
-    const user: User = await this.userRepository.findOne({
-      where: { id: userId },
-    });
-
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
-
-    let order = {};
-    const where: any = { user: { id: userId } };
-
-    if (orderQuery.order) {
-      order =
-        orderQuery.order === 'crescente'
-          ? { createdAt: 'ASC' }
-          : { createdAt: 'DESC' };
-    }
-
-    if (orderQuery.statusEnvio) {
-      where.statusEnvio = orderQuery.statusEnvio;
-    }
-
-    if (orderQuery.statusCorrecao) {
-      where.statusCorrecao =
-        orderQuery.statusCorrecao === 'corrigidas'
-          ? 'corrigida'
-          : 'nao_corrigida';
-    }
-
-    const redacoes: Redacao[] = await this.redacaoRepository.find({
-      where,
-      order,
-      take: limit,
-      skip: offset,
-    });
-
-    const totalRedacoes = await this.redacaoRepository.count({ where });
-
-    if (redacoes.length === 0) {
-      throw new NotFoundException('Redacoes not found');
-    }
-
-    return { redacoes, totalRedacoes };
   }
 
   async getRedacaoById(userId: string, id: number) {
-    if (!userId) {
-      throw new NotFoundException('User not found');
+    try {
+      if (!userId) {
+        throw new NotFoundException('User not found');
+      }
+
+      if (!id || isNaN(id)) {
+        throw new BadRequestException('Invalid id');
+      }
+
+      const user: User = await this.userRepository.findOne({
+        where: { id: userId },
+      });
+
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      const redacao: Redacao = await this.redacaoRepository.findOne({
+        where: { id, user: { id: userId } },
+      });
+
+      if (!redacao) {
+        throw new NotFoundException('Redacao not found');
+      }
+
+      return redacao;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-
-    if (!id || isNaN(id)) {
-      throw new BadRequestException('Invalid id');
-    }
-
-    const user: User = await this.userRepository.findOne({
-      where: { id: userId },
-    });
-
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
-
-    const redacao: Redacao = await this.redacaoRepository.findOne({
-      where: { id, user: { id: userId } },
-    });
-
-    if (!redacao) {
-      throw new NotFoundException('Redacao not found');
-    }
-
-    return redacao;
   }
 }
