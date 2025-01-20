5678635e83b86e751cdbe93cc2f397b33acc840c
Luis-Moura <luish.moura2024@gmail.com>
2025-01-20 11:37:10 -0300

    test(redacao): adiciona testes para o serviço do modulo de redações

--- src/redacoes/services/createRedacao.service.spec.ts
+++ src/redacoes/services/createRedacao.service.spec.ts
@@ -3,7 +3,7 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
@@ -12,29 +12,56 @@ import { createDraftRedacaoDto } from '../dto/createDraftRedacaoDto';
 import { Redacao } from '../entities/redacao.entity';
 import { CreateRedacaoService } from './createRedacao.service';
 
+const userEntity = new User({
+  id: 'userId',
+  name: 'Test User',
+  email: 'test@example.com',
+  password: 'password',
+  verified: true,
+  role: 'user',
+  twoFA: false,
+  failedLoginAttempts: 0,
+  lockUntil: null,
+  verificationCode: null,
+  verificationCodeExpires: null,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  redacoes: [],
+});
+
+const redacaoEntity = new Redacao({
+  id: 1,
+  content: 'test content',
+  title: 'test title',
+  topic: 'test topic',
+  createdAt: new Date(),
+  statusCorrecao: 'status test',
+  statusEnvio: 'status test',
+  updatedAt: new Date(),
+  user: userEntity,
+});
+
 describe('CreateRedacaoService', () => {
   let service: CreateRedacaoService;
   let redacaoRepository: Repository<Redacao>;
   let userRepository: Repository<User>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module = await Test.createTestingModule({
       providers: [
         CreateRedacaoService,
         {
           provide: getRepositoryToken(Redacao),
           useValue: {
-            create: jest.fn(), // Adiciona o mock do método create
-            save: jest.fn(), // Adiciona o mock do método save
-            findOne: jest.fn(), // Adiciona o mock do método findOne
-            merge: jest.fn(), // Adiciona o mock do método merge
+            findOne: jest.fn().mockResolvedValue(redacaoEntity),
+            create: jest.fn().mockReturnValue(redacaoEntity),
+            save: jest.fn().mockResolvedValue(redacaoEntity),
+            merge: jest.fn().mockReturnValue(redacaoEntity),
           },
         },
         {
           provide: getRepositoryToken(User),
-          useValue: {
-            findOne: jest.fn(),
-          },
+          useValue: { findOne: jest.fn().mockResolvedValue(userEntity) },
         },
       ],
     }).compile();
@@ -46,118 +73,175 @@ describe('CreateRedacaoService', () => {
     userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(redacaoRepository).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
   describe('createDefinitiveRedacao', () => {
+    it('should create a definitive redacao', async () => {
+      const redacaoDto: createDefinitiveRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
+      const result = await service.createDefinitiveRedacao(
+        redacaoDto,
+        'userId',
+      );
+
+      expect(result).toEqual(redacaoEntity);
+      expect(redacaoRepository.create).toHaveBeenCalledTimes(1);
+      expect(redacaoRepository.save).toHaveBeenCalledTimes(1);
+      expect(userRepository.findOne).toHaveBeenCalledTimes(1);
+    });
+
     it('should throw NotFoundException if userId is not provided', async () => {
+      const redacaoDto: createDefinitiveRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
       await expect(
-        service.createDefinitiveRedacao({} as createDefinitiveRedacaoDto, ''),
+        service.createDefinitiveRedacao(redacaoDto, ''),
       ).rejects.toThrow(NotFoundException);
     });
 
     it('should throw NotFoundException if user is not found', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
+      jest.spyOn(userRepository, 'findOne').mockResolvedValueOnce(null);
+
+      const redacaoDto: createDefinitiveRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
       await expect(
-        service.createDefinitiveRedacao({} as createDefinitiveRedacaoDto, '1'),
+        service.createDefinitiveRedacao(redacaoDto, 'userId'),
       ).rejects.toThrow(NotFoundException);
     });
 
-    it('should throw NotFoundException if redacao is not found', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue({} as User);
-      jest.spyOn(redacaoRepository, 'findOne').mockResolvedValue(null);
+    it('should throw NotFoundException if redacao is not found when updating', async () => {
+      jest.spyOn(redacaoRepository, 'findOne').mockResolvedValueOnce(null);
+
+      const redacaoDto: createDefinitiveRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
       await expect(
-        service.createDefinitiveRedacao(
-          {} as createDefinitiveRedacaoDto,
-          '1',
-          1,
-        ),
+        service.createDefinitiveRedacao(redacaoDto, 'userId', 1),
       ).rejects.toThrow(NotFoundException);
     });
 
     it('should throw BadRequestException if redacao is already sent', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue({} as User);
-      jest
-        .spyOn(redacaoRepository, 'findOne')
-        .mockResolvedValue({ statusEnvio: 'enviado' } as Redacao);
+      jest.spyOn(redacaoRepository, 'findOne').mockResolvedValueOnce({
+        ...redacaoEntity,
+        statusEnvio: 'enviado',
+      });
+
+      const redacaoDto: createDefinitiveRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
       await expect(
-        service.createDefinitiveRedacao(
-          {} as createDefinitiveRedacaoDto,
-          '1',
-          1,
-        ),
+        service.createDefinitiveRedacao(redacaoDto, 'userId', 1),
       ).rejects.toThrow(BadRequestException);
     });
 
-    it('should save a new redacao if redacaoId is not provided', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue({} as User);
-      jest.spyOn(redacaoRepository, 'create').mockReturnValue({} as Redacao);
-      jest.spyOn(redacaoRepository, 'save').mockResolvedValue({} as Redacao);
-
-      await expect(
-        service.createDefinitiveRedacao({} as createDefinitiveRedacaoDto, '1'),
-      ).resolves.toEqual({});
-    });
-
-    it('should merge and save an existing redacao if redacaoId is provided', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue({} as User);
-      jest
-        .spyOn(redacaoRepository, 'findOne')
-        .mockResolvedValue({ statusEnvio: 'rascunho' } as Redacao);
-      jest.spyOn(redacaoRepository, 'merge').mockReturnValue({} as Redacao);
-      jest.spyOn(redacaoRepository, 'save').mockResolvedValue({} as Redacao);
-
-      await expect(
-        service.createDefinitiveRedacao(
-          {} as createDefinitiveRedacaoDto,
-          '1',
-          1,
-        ),
-      ).resolves.toEqual({});
-    });
-
     it('should throw InternalServerErrorException if save fails', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue({} as User);
       jest
         .spyOn(redacaoRepository, 'save')
-        .mockRejectedValue(new Error('Save failed'));
+        .mockRejectedValueOnce(new Error('Save failed'));
+
+      const redacaoDto: createDefinitiveRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
 
       await expect(
-        service.createDefinitiveRedacao({} as createDefinitiveRedacaoDto, '1'),
+        service.createDefinitiveRedacao(redacaoDto, 'userId'),
       ).rejects.toThrow(InternalServerErrorException);
     });
   });
 
   describe('createDraft', () => {
+    it('should create a draft redacao', async () => {
+      const redacaoDto: createDraftRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
+      const result = await service.createDraft('userId', redacaoDto);
+
+      expect(result).toEqual(redacaoEntity);
+      expect(redacaoRepository.create).toHaveBeenCalledTimes(1);
+      expect(redacaoRepository.save).toHaveBeenCalledTimes(1);
+      expect(userRepository.findOne).toHaveBeenCalledTimes(1);
+    });
+
     it('should throw NotFoundException if userId is not provided', async () => {
-      await expect(
-        service.createDraft('', {} as createDraftRedacaoDto),
-      ).rejects.toThrow(NotFoundException);
+      const redacaoDto: createDraftRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
+      await expect(service.createDraft('', redacaoDto)).rejects.toThrow(
+        NotFoundException,
+      );
     });
 
     it('should throw NotFoundException if user is not found', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
+      jest.spyOn(userRepository, 'findOne').mockResolvedValueOnce(null);
+
+      const redacaoDto: createDraftRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
+      await expect(service.createDraft('userId', redacaoDto)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+
+    it('should throw NotFoundException if redacao is not found when updating', async () => {
+      jest.spyOn(redacaoRepository, 'findOne').mockResolvedValueOnce(null);
+
+      const redacaoDto: createDraftRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
       await expect(
-        service.createDraft('1', {} as createDraftRedacaoDto),
+        service.createDraft('userId', redacaoDto, 1),
       ).rejects.toThrow(NotFoundException);
     });
 
-    it('should save a new draft redacao', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue({} as User);
-      jest.spyOn(redacaoRepository, 'create').mockReturnValue({} as Redacao);
-      jest.spyOn(redacaoRepository, 'save').mockResolvedValue({} as Redacao);
-
-      await expect(
-        service.createDraft('1', {} as createDraftRedacaoDto),
-      ).resolves.toEqual({});
-    });
-
     it('should throw InternalServerErrorException if save fails', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue({} as User);
       jest
         .spyOn(redacaoRepository, 'save')
-        .mockRejectedValue(new Error('Save failed'));
+        .mockRejectedValueOnce(new Error('Save failed'));
 
-      await expect(
-        service.createDraft('1', {} as createDraftRedacaoDto),
-      ).rejects.toThrow(InternalServerErrorException);
+      const redacaoDto: createDraftRedacaoDto = {
+        content: 'test content',
+        title: 'test title',
+        topic: 'test topic',
+      };
+
+      await expect(service.createDraft('userId', redacaoDto)).rejects.toThrow(
+        InternalServerErrorException,
+      );
     });
   });
 });
--- src/redacoes/services/getRedacao.service.spec.ts
+++ src/redacoes/services/getRedacao.service.spec.ts
@@ -1,39 +1,62 @@
 import { BadRequestException, NotFoundException } from '@nestjs/common';
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Redacao } from 'src/redacoes/entities/redacao.entity';
-import { GetRedacaoService } from 'src/redacoes/services/getRedacao.service';
 import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { Redacao } from '../entities/redacao.entity';
+import { GetRedacaoService } from './getRedacao.service';
 
-describe('GetRedacaoService', () => {
+const userEntity = new User({
+  id: 'userId',
+  name: 'Test User',
+  email: 'test@example.com',
+  password: 'password',
+  verified: true,
+  role: 'user',
+  twoFA: false,
+  failedLoginAttempts: 0,
+  lockUntil: null,
+  verificationCode: null,
+  verificationCodeExpires: null,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  redacoes: [],
+});
+
+const redacaoEntity = [
+  new Redacao({
+    id: 1,
+    content: 'test content',
+    title: 'test title',
+    topic: 'test topic',
+    createdAt: new Date(),
+    statusCorrecao: 'status test',
+    statusEnvio: 'status test',
+    updatedAt: new Date(),
+    user: userEntity,
+  }),
+];
+
+describe('getRedacao', () => {
   let service: GetRedacaoService;
   let redacaoRepository: Repository<Redacao>;
   let userRepository: Repository<User>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const module = await Test.createTestingModule({
       providers: [
         GetRedacaoService,
         {
           provide: getRepositoryToken(Redacao),
           useValue: {
-            findOne: jest.fn(),
-            find: jest.fn(),
-            save: jest.fn(),
-            create: jest.fn(),
-            merge: jest.fn(),
-            count: jest.fn(),
+            find: jest.fn().mockResolvedValue(redacaoEntity),
+            count: jest.fn().mockResolvedValue(1),
+            findOne: jest.fn().mockResolvedValue(redacaoEntity[0]),
           },
         },
         {
           provide: getRepositoryToken(User),
-          useValue: {
-            findOne: jest.fn(),
-            find: jest.fn(),
-            save: jest.fn(),
-            count: jest.fn(),
-          },
+          useValue: { findOne: jest.fn().mockResolvedValue(userEntity) },
         },
       ],
     }).compile();
@@ -45,78 +68,75 @@ describe('GetRedacaoService', () => {
     userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
-  describe('getRedacoes', () => {
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(redacaoRepository).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  describe('getRedacao', () => {
+    it('should return a redacao', async () => {
+      const result = await service.getRedacoes('userId', 10, 0, {
+        order: 'crescente',
+        statusEnvio: 'rascunho',
+        statusCorrecao: 'nao-corrigidas',
+      });
+
+      expect(result).toEqual({ redacoes: redacaoEntity, totalRedacoes: 1 });
+      expect(redacaoRepository.find).toHaveBeenCalledTimes(1);
+      expect(userRepository.findOne).toHaveBeenCalledTimes(1);
+    });
+
     it('should throw NotFoundException if userId is not provided', async () => {
-      await expect(service.getRedacoes('', 10, 0, {})).rejects.toThrow(
-        NotFoundException,
-      );
+      await expect(
+        service.getRedacoes('', 10, 0, {
+          order: 'crescente',
+          statusEnvio: 'rascunho',
+          statusCorrecao: 'nao-corrigidas',
+        }),
+      ).rejects.toThrow(NotFoundException);
     });
 
     it('should throw BadRequestException if limit or offset is invalid', async () => {
-      await expect(service.getRedacoes('userId', NaN, 0, {})).rejects.toThrow(
-        BadRequestException,
-      );
-      await expect(service.getRedacoes('userId', 10, NaN, {})).rejects.toThrow(
-        BadRequestException,
-      );
+      await expect(
+        service.getRedacoes('userId', NaN, 0, {
+          order: 'crescente',
+          statusEnvio: 'rascunho',
+          statusCorrecao: 'nao-corrigidas',
+        }),
+      ).rejects.toThrow(BadRequestException);
+
+      await expect(
+        service.getRedacoes('userId', 10, NaN, {
+          order: 'crescente',
+          statusEnvio: 'rascunho',
+          statusCorrecao: 'nao-corrigidas',
+        }),
+      ).rejects.toThrow(BadRequestException);
     });
 
     it('should throw NotFoundException if user is not found', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
-      await expect(service.getRedacoes('userId', 10, 0, {})).rejects.toThrow(
-        NotFoundException,
-      );
+      jest.spyOn(userRepository, 'findOne').mockResolvedValueOnce(null);
+
+      await expect(
+        service.getRedacoes('userId', 10, 0, {
+          order: 'crescente',
+          statusEnvio: 'rascunho',
+          statusCorrecao: 'nao-corrigidas',
+        }),
+      ).rejects.toThrow(NotFoundException);
     });
 
-    it('should return redacoes', async () => {
-      const user = new User();
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(user);
-      const redacoes = [new Redacao()];
-      jest.spyOn(redacaoRepository, 'find').mockResolvedValue(redacoes);
-      jest.spyOn(redacaoRepository, 'count').mockResolvedValue(1);
+    it('should throw NotFoundException if no redacoes are found', async () => {
+      jest.spyOn(redacaoRepository, 'find').mockResolvedValueOnce([]);
 
-      const result = await service.getRedacoes('userId', 10, 0, {});
-      expect(result).toEqual({ redacoes, totalRedacoes: 1 });
-    });
-  });
-
-  describe('getRedacaoById', () => {
-    it('should throw NotFoundException if userId is not provided', async () => {
-      await expect(service.getRedacaoById('', 1)).rejects.toThrow(
-        NotFoundException,
-      );
-    });
-
-    it('should throw BadRequestException if id is invalid', async () => {
-      await expect(service.getRedacaoById('userId', NaN)).rejects.toThrow(
-        BadRequestException,
-      );
-    });
-
-    it('should throw NotFoundException if user is not found', async () => {
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(null);
-      await expect(service.getRedacaoById('userId', 1)).rejects.toThrow(
-        NotFoundException,
-      );
-    });
-
-    it('should throw NotFoundException if redacao is not found', async () => {
-      const user = new User();
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(user);
-      jest.spyOn(redacaoRepository, 'findOne').mockResolvedValue(null);
-      await expect(service.getRedacaoById('userId', 1)).rejects.toThrow(
-        NotFoundException,
-      );
-    });
-
-    it('should return redacao', async () => {
-      const user = new User();
-      jest.spyOn(userRepository, 'findOne').mockResolvedValue(user);
-      const redacao = new Redacao();
-      jest.spyOn(redacaoRepository, 'findOne').mockResolvedValue(redacao);
-
-      const result = await service.getRedacaoById('userId', 1);
-      expect(result).toEqual(redacao);
+      await expect(
+        service.getRedacoes('userId', 10, 0, {
+          order: 'crescente',
+          statusEnvio: 'rascunho',
+          statusCorrecao: 'nao-corrigidas',
+        }),
+      ).rejects.toThrow(NotFoundException);
     });
   });
 });
