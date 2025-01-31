0cc0025c666318ba9edbc87e11f08ab71e984cb0
Luis-Moura <luish.moura2024@gmail.com>
2025-01-31 08:06:45 -0300

    feat(correcoes): adiciona controlador e serviço para deletar correções por ID

---
+++ src/correcoesModule/Correcoes/controller/deleteCorrecoes.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Delete, Param, Request, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { DeleteCorrecoesService } from '../service/deleteCorrecoes.service';
+
+@ApiTags('correcao')
+@Controller('delete-correcao')
+export class DeleteCorrecoesController {
+  constructor(
+    private readonly deleteCorrecoesService: DeleteCorrecoesService,
+  ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':correcaoId')
+  async deleteCorrecaoById(
+    @Param('correcaoId') correcaoId: number,
+    @Request() req,
+  ) {
+    const corretor = req.user.userId;
+
+    return this.deleteCorrecoesService.deleteCorrecaoById(corretor, correcaoId);
+  }
+}
--- src/correcoesModule/correcoes.module.ts
+++ src/correcoesModule/correcoes.module.ts
@@ -3,14 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Redacao } from 'src/redacoes/entities/redacao.entity';
 import { User } from 'src/users/entities/user.entity';
 import { CreateCorrecoesController } from './Correcoes/controller/createCorrecoes.controller';
+import { DeleteCorrecoesController } from './Correcoes/controller/deleteCorrecoes.controller';
 import { GetCorrecoesController } from './Correcoes/controller/getRedacoes.controller';
 import { CreateCorrecoesService } from './Correcoes/service/createCorrecoes.service';
+import { DeleteCorrecoesService } from './Correcoes/service/deleteCorrecoes.service';
 import { GetCorrecoesService } from './Correcoes/service/getRedacoes.service';
 import { Correcao } from './entities/correcao.entity';
 
 @Module({
-  controllers: [CreateCorrecoesController, GetCorrecoesController],
-  providers: [CreateCorrecoesService, GetCorrecoesService],
+  controllers: [
+    CreateCorrecoesController,
+    GetCorrecoesController,
+    DeleteCorrecoesController,
+  ],
+  providers: [
+    CreateCorrecoesService,
+    GetCorrecoesService,
+    DeleteCorrecoesService,
+  ],
   imports: [TypeOrmModule.forFeature([Redacao, User, Correcao])],
 })
 export class CorrecoesModule {}
