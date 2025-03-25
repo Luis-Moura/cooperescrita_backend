da03048950b006ef434ef3cec5ff429011e662cc
Luis-Moura <luish.moura2024@gmail.com>
2025-03-25 15:07:39 -0300

    refactor(auth): simplifica validações de senha no DTO de login

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,12 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignInDto {
   @ApiProperty({ description: 'Email do usuário' })
@@ -15,20 +8,6 @@ export class SignInDto {
   email: string;
 
   @ApiProperty({ description: 'Senha do usuário', minLength: 8, maxLength: 20 })
-  @MinLength(8, { message: 'Password must be at least 8 characters long' })
-  @MaxLength(20, { message: 'Password must be at most 20 characters long' })
-  @Matches(/(?=.*[A-Z])/, {
-    message: 'Password must contain at least one uppercase letter',
-  })
-  @Matches(/(?=.*[a-z])/, {
-    message: 'Password must contain at least one lowercase letter',
-  })
-  @Matches(/(?=.*[0-9])/, {
-    message: 'Password must contain at least one number',
-  })
-  @Matches(/(?=.*[!@#$%^&*])/, {
-    message: 'Password must contain at least one special character',
-  })
   @IsString()
   @IsNotEmpty()
   password: string;
